43b171861110fdc20b0b237c41c6bcadd34f1c46
Henrique Gonçalves <henriqinho20@gmail.com>
2023-12-11 04:26:32 +0000

    Inital commit with prettier, husky, eslint

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+// const api = require('./api');
+
+// require('dotenv').config();
+
+const dev = process.env.NODE_ENV !== 'production';
+// const MYSQL_URL = dev ? process.env.MYSQL_URL_TEST : process.env.MYSQL_URL;
